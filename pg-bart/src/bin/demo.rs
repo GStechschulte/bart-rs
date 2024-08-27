22159a83db1cccbe0cb080eb24bd3526c4b3a15d
GStechschulte <stechschulteg@gmail.com>
2024-08-27 21:59:31 +0200

    remove unused code

--- pg-bart/src/bin/demo.rs
+++ pg-bart/src/bin/demo.rs
@@ -13,29 +13,5 @@ struct Indices {
 }
 
 fn main() {
-    // let sampler = ParticleGibbsSampler::new(10, 5);
-    // println!("{:?}", sampler);
-
-    let a2 = array![[1, 2], [3, 4]];
-
-    let matrix = vec![
-        vec![1.0, 2.0, 3.0],
-        vec![4.0, 5.0, 6.0],
-        vec![7.0, 8.0, 9.0],
-    ];
-
-    println!("{:?}", matrix);
-
-    let mut rng = rand::thread_rng();
-    println!("{:?}", rng);
-
-    let n_points = 10 as usize;
-    let n: Vec<usize> = Vec::from_iter(0..n_points);
-    let indices = Indices {
-        leaf_nodes: HashSet::from([0]),
-        expansion_nodes: VecDeque::from([0]),
-        data_indices: HashMap::from([(0, n)]),
-    };
-
-    println!("{:?}", indices);
+    println!("Hello BART")
 }
