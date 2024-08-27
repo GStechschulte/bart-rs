16d6730d1203a8ccdf389886159bff92264a1321
GStechschulte <stechschulteg@gmail.com>
2024-08-27 22:00:22 +0200

    add Matrix struct

--- pg-bart/src/data.rs
+++ pg-bart/src/data.rs
@@ -1,7 +1,8 @@
 // Represents the training data used to fit a Binary Decision Tree
 //
-// TODO: Use Arrow
+// TODO: Use Arrow?
 
+/// Represents a matrix from a vector of data
 pub struct Matrix<T>
 where
     T: Copy,
@@ -15,6 +16,7 @@ impl<T> Matrix<T>
 where
     T: Copy,
 {
+    /// Creates a new matrix from a vector of data
     pub fn from_vec(data: Vec<T>, n_rows: usize, n_cols: usize) -> Self {
         if data.len() != (n_rows * n_cols) {
             panic!("Data size does not match the provided dimension");
@@ -27,10 +29,12 @@ where
         }
     }
 
+    /// Gets the value at the specified row and column
     pub fn get(&self, i: &usize, j: &usize) -> T {
         self.data[i * self.n_cols + j]
     }
 
+    /// Selects the values at the specified rows and column
     pub fn select_rows(&self, rows: &Vec<usize>, col: &usize) -> Vec<T> {
         let mut ret = Vec::<T>::with_capacity(rows.len());
 
@@ -40,4 +44,9 @@ where
 
         ret
     }
+}
+
+pub trait ExternalData {
+    fn X(&self) -> &Matrix<f64>;
+    fn y(&self) -> &Vec<f64>;
 }
--- pg-bart/src/lib.rs
+++ pg-bart/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod data;
 pub mod particle;
 pub mod sampler;
+pub mod split_rules;
 pub mod tree;
