54a33aa525076179274e3feb5cad0ade92e99d8a
GStechschulte <stechschulteg@gmail.com>
2024-09-04 08:15:03 +0200

    clone X and y arrays on getter

--- bindings/src/data.rs
+++ bindings/src/data.rs
@@ -6,31 +6,31 @@ use ndarray::{Array1, Array2};
 use numpy::{PyArrayMethods, PyReadonlyArray1, PyReadonlyArray2};
 use pg_bart::data::PyData;
 
-// TODO: Is this necessary?
-// Lifetime annotation ensures PyReadonlyArray refereces
-// do not outlive ExternalData. ExternalData lives for the
-// lifetime of the PyObject
-pub struct ExternalData<'py> {
-    X: PyReadonlyArray2<'py, f64>,
-    y: PyReadonlyArray1<'py, f64>,
+pub struct ExternalData {
+    X: Array2<f64>,
+    y: Array1<f64>,
 }
 
-impl<'py> ExternalData<'py> {
-    pub fn new(X: PyReadonlyArray2<'py, f64>, y: PyReadonlyArray1<'py, f64>) -> Self {
-        ExternalData { X, y }
+impl ExternalData {
+    pub fn new(X: PyReadonlyArray2<f64>, y: PyReadonlyArray1<f64>) -> Self {
+        ExternalData {
+            // `.to_owned_array()` creates a copy of X and y
+            X: X.to_owned_array(),
+            y: y.to_owned_array(),
+        }
     }
 }
 
-impl<'py> PyData for ExternalData<'py> {
+impl PyData for ExternalData {
     fn X(&self) -> Array2<f64> {
-        self.X.to_owned_array()
+        self.X.clone()
     }
 
     fn y(&self) -> Array1<f64> {
-        self.y.to_owned_array()
+        self.y.clone()
     }
 
     fn model_logp(&self, v: Array1<f64>) -> f64 {
-        todo!("Implement model_logp...")
+        todo!("Implement model_logp")
     }
 }
--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -13,14 +13,10 @@ use pyo3::prelude::*;
 fn shape(py: Python, arr: PyReadonlyArray2<f64>) -> PyResult<PyObject> {
     let arr_view = arr.as_array();
     println!("arr.as_array(): {:?}", arr_view);
-    let data = arr_view.as_standard_layout().into_owned().into_raw_vec();
-    println!("data: {:?}", data[0 * 3 + 1]);
+    println!("{:?}", arr_view.mean());
+    // let data = arr_view.as_standard_layout().into_owned().into_raw_vec();
+    // println!("data: {:?}", data[0 * 3 + 1]);
     Ok(PyArray2::from_array_bound(py, &arr_view).to_object(py))
-}
-
-#[pyfunction]
-fn initialize(X: PyReadonlyArray2<f64>, y: PyReadonlyArray1<f64>) {
-    let data = ExternalData::new(X, y);
 }
 
 #[pyclass(unsendable)]
@@ -29,7 +25,7 @@ struct StateWrapper {
 }
 
 #[pyfunction]
-fn initialize_bart(
+fn initialize(
     X: PyReadonlyArray2<f64>,
     y: PyReadonlyArray1<f64>,
     logp: usize,
@@ -55,11 +51,9 @@ fn initialize_bart(
     StateWrapper { state }
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn bart_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(shape, m)?)?;
     m.add_function(wrap_pyfunction!(initialize, m)?)?;
-    m.add_function(wrap_pyfunction!(initialize_bart, m)?)?;
     Ok(())
 }
