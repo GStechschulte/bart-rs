1f9631cb4a3f7c1e10fa77db167be666b65f4f58
GStechschulte <stechschulteg@gmail.com>
2024-10-05 10:11:35 +0200

    split rule interface

--- pg-bart/src/split.rs
+++ pg-bart/src/split.rs
@@ -1,23 +1,71 @@
-// TODO:    Implement different split rule traits for the DecisionTree
-//          as the user can pass a `split_rules` argument
-//          `split_rules` is either `ContinuousSplitRule`, `OneHotSplitRule`
-//          or `SubsetSplitRule`.
-//
-//          The code below is simply a template!!!
+use rand::Rng;
+use std::{collections::HashSet, f64};
 
-// pub trait SplitRule {
-//     fn get_split_value(&self, values: &[f64]) -> Option<f64>;
-//     fn divide(&self, values: &[f64], split_value: f64) -> Vec<bool>;
-// }
+/// Interface for split strategies.
+pub trait SplitRule {
+    type T;
 
-// pub struct ContinuousSplitRule;
+    fn get_split_value(&self, candidates: &[Self::T]) -> Option<Self::T>;
+    fn divide(&self, candidates: &[Self::T], split_value: Self::T) -> Vec<bool>;
+}
 
-// impl SplitRule for ContinuousSplitRule {
-//     fn get_split_value(&self, values: &[f64]) -> Option<f64> {
-//         todo!("Implement continuous split value selection");
-//     }
+/// Standard continuous split rule. Pick a pivot value and split
+/// depending on if variable is smaller or greater than the value picked.
+pub struct ContinuousSplit;
 
-//     fn divide(&self, values: &[f64], split_value: f64) -> Vec<bool> {
-//         todo!("Implement continuous split")
-//     }
-// }
+impl SplitRule for ContinuousSplit {
+    type T = f64;
+
+    fn get_split_value(&self, candidates: &[f64]) -> Option<f64> {
+        if candidates.len() > 1 {
+            let idx = rand::thread_rng().gen_range(0..candidates.len());
+            Some(candidates[idx])
+        } else {
+            None
+        }
+    }
+
+    fn divide(&self, candidates: &[f64], split_value: f64) -> Vec<bool> {
+        candidates.iter().map(|&x| x <= split_value).collect()
+    }
+}
+
+/// Choose a single categorical value and branch on it if the variable is that value or not.
+pub struct OneHotSplit;
+
+impl SplitRule for OneHotSplit {
+    // TODO: macro to generate an implementation for multple integer types?
+    type T = i32;
+
+    fn get_split_value(&self, candidates: &[i32]) -> Option<i32> {
+        if candidates.len() > 1 && !candidates.iter().all(|&x| x == candidates[0]) {
+            let idx = rand::thread_rng().gen_range(0..candidates.len());
+            Some(candidates[idx])
+        } else {
+            None
+        }
+    }
+
+    fn divide(&self, candidates: &[i32], split_value: i32) -> Vec<bool> {
+        candidates.iter().map(|&x| x == split_value).collect()
+    }
+}
+
+/// Choose a random subset of the categorical values and branch on belonging to that set.
+///
+/// This is the approach taken by Sameer K. Deshpande.
+/// flexBART: Flexible Bayesian regression trees with categorical predictors. arXiv,
+/// `link <https://arxiv.org/abs/2211.04459>`__
+pub struct SubsetSplit;
+
+impl SplitRule for SubsetSplit {
+    type T = i32;
+
+    fn get_split_value(&self, candidates: &[i32]) -> Option<i32> {
+        todo!("Implement")
+    }
+
+    fn divide(&self, candidates: &[i32], split_value: i32) -> Vec<bool> {
+        todo!("Implement")
+    }
+}
