56df8990cfb79104cb265253880d2cccdd266b9a
GStechschulte <stechschulteg@gmail.com>
2024-11-19 14:28:37 +0100

    update evaluate_logp fn to reflect new LogPFunc signature

--- bindings/src/data.rs
+++ bindings/src/data.rs
@@ -14,40 +14,21 @@ use pg_bart::data::PyData;
 // have the same representation as the C type
 type LogpFunc = unsafe extern "C" fn(*const f64, usize) -> c_double;
 
-// type LogpFunc = unsafe extern "C" fn(
-//     dim: c_uint,
-//     x: *const c_double,
-//     out: *mut c_double,
-//     logp: *mut c_double,
-//     user_data: *mut c_void,
-// ) -> i64;
-
 pub struct ExternalData {
     X: Array2<f64>,
     y: Array1<f64>,
     logp: LogpFunc,
-    // n_dim: usize,
-    // user_data: *mut c_void,
 }
 
 impl ExternalData {
-    pub fn new(
-        X: PyReadonlyArray2<f64>,
-        y: PyReadonlyArray1<f64>,
-        logp: usize,
-        // n_dim: usize,
-        // user_data: usize,
-    ) -> Self {
+    pub fn new(X: PyReadonlyArray2<f64>, y: PyReadonlyArray1<f64>, logp: usize) -> Self {
         let logp: LogpFunc = unsafe { std::mem::transmute(logp as *const c_void) };
-        // let user_data = user_data as *mut c_void;
 
         Self {
             // `.to_owned_array()` creates a copy of X and y
             X: X.to_owned_array(),
             y: y.to_owned_array(),
             logp,
-            // n_dim,
-            // user_data,
         }
     }
 }
@@ -63,42 +44,7 @@ impl PyData for ExternalData {
     }
 
     fn evaluate_logp(&self, x: Array1<f64>) -> f64 {
-        // let mut ll = 0.0;
         let logp = unsafe { (self.logp)(x.as_ptr(), x.len()) };
         logp
-
-        // unsafe {
-        //     let result = (self.logp)(x.as_ptr(), &mut ll, x.len() as c_uint);
-        //     result
-        // }
     }
-
-    // println!("x.len(): {:?}, self.n_dim: {:?}", x.len(), self.n_dim);
-    // if x.len() != self.n_dim {
-    //     return Err("Input dimension mismatch");
-    // }
-
-    // let mut logp = 0.0;
-    // let mut gradient = vec![0.0; self.n_dim];
-
-    // unsafe {
-    //     let result = (self.logp)(
-    //         self.n_dim as c_uint,
-    //         x.as_ptr(),
-    //         gradient.as_mut_ptr(),
-    //         &mut logp,
-    //         self.user_data,
-    //     );
-
-    //     // Codes used from nutpie `compile_pymc.py`.
-    //     // TODO: Link to code
-    //     match result {
-    //         0 => Ok((logp, gradient)),
-    //         1 => Err("Unknown exception occurred"),
-    //         3 => Err("Gradient contains non-finite values"),
-    //         4 => Err("Log probability is non-finite"),
-    //         _ => Err("Unknown error occurred"),
-    //     }
-    //         }
-    //     }
 }
